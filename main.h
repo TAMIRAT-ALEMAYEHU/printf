db704a258497d907621b0491aae0e72253ddbe3b
Tamirat <88383918+TAMIRAT-ALEMAYEHU@users.noreply.github.com>
2021-10-30 00:11:48 +0300

    edited

--- main.h
+++ main.h
@@ -1,36 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdarg.h>
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
- *
- * Return: int
- */
-typedef struct structprint
-{
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
-
-int _putchar(char ch);
-int _puts(char *string);
-int printc(char *format, va_list);
-int printstr(char *format, va_list);
-int (*driver(char *format))(char *format, va_list);
+#include <stdint.h>
+int _putchar(char c);
+int print_number(int n);
 int _printf(const char *format, ...);
-int printint(char *format, va_list pa);
-int integer(int number);
-int contadordigit(int number);
-int _abs(int number);
-int printpercent(char *format, va_list pa);
-int printhex(char *format, va_list);
-int printHEX(char *format, va_list);
-int printocta(char *format, va_list);
-int print_unsign(char *format, va_list);
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *);
+int print_hl(uintptr_t n);
 #endif
